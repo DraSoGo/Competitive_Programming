2ef5aaa514c258e13cc27861c893c160c7c916fc
DraSoGo <133212388+DraSoGo@users.noreply.github.com>
2026-07-17 10:51:32 +0700

    Remove debug output for time calculations

--- Ovenbreak/Grader/A20-0010_croissant_isnt_robot_t_tong_ton_tak_dad_tang_wan.cpp
+++ Ovenbreak/Grader/A20-0010_croissant_isnt_robot_t_tong_ton_tak_dad_tang_wan.cpp
@@ -7,7 +7,6 @@ int main()
     cin >> s >> h1 >> m1 >> h2 >> m2;
     int t1 = stoi(h1) * 60 + stoi(m1);
     int t2 = stoi(h2) * 60 + stoi(m2);
-    cout << t1 << " " << t2 << endl;
     int m = (720-t1) + t2;
     int h = m / 60;
     m %= 60;
